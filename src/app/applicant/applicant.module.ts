908761087644cc0f718923be5e3eaec06ebefbfc
AlfikaNM <alfikanmuis@gmail.com>
2022-10-23 13:58:38 +0700

    setup

--- src/app/applicant/applicant.module.ts
+++ src/app/applicant/applicant.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileUpdateComponent } from './profile-update/profile-update.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
+    ProfileComponent,
+    ProfileUpdateComponent,
   ],
   imports: [
     CommonModule,
