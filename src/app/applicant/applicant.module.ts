be36dc01cf2bd4b84ca93a25e7c22c8a369d5ea1
AlfikaNM <alfikanmuis@gmail.com>
2022-10-23 13:34:26 +0700

    setup

--- src/app/applicant/applicant.module.ts
+++ src/app/applicant/applicant.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     HomepageComponent,
+    ProfileComponent,
   ],
   imports: [
     CommonModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    // HeaderComponent,
+    // FooterComponent
   ],
   imports: [
     CommonModule,
